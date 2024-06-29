bd337ca4694e4f4cf65876deab7b1ccd187cde1c
CasimirKaPazi <jul.lutz@gmx.net>
2024-06-29 14:53:03 +0200

    Change the effect of memory

--- main.lua
+++ main.lua
@@ -54,7 +54,7 @@ function updateActivation()
 				for y = -RANGE, RANGE do
 					if x ~= 0 or y ~= 0 then
 						local nI, nJ = wrapAroundGrid(i + x, j + y)
-						sum = sum + grid[nI][nJ].past_act * grid[i][j].past_weights[w]
+						sum = sum + grid[nI][nJ].past_act * grid[i][j].past_weights[w] * grid[i][j].memory
 						sum = sum + grid[nI][nJ].act * grid[i][j].weights[w]
 						w = w+1
 					end
@@ -64,7 +64,7 @@ function updateActivation()
 			grid[i][j].past2_act = grid[i][j].past_act
 			grid[i][j].past_act = currentact
 			-- Update activation using ReLU
-			sum = math.max(0, (sum*3 + currentact*grid[i][j].memory)/(3+grid[i][j].memory))
+			sum = math.max(0, sum)
 			if sum >= MAX_ACT then sum = 0 end
 			average_sum = average_sum + sum
 			grid[i][j].new_act = sum
@@ -86,9 +86,9 @@ function updateWeights()
 			for w = 1, N_WEIGHTS do
 				local nI, nJ = wrapAroundGrid(i + weight_pos[w].x, j + weight_pos[w].y)
 				grid[i][j].past_weights[w] = grid[i][j].past_weights[w] +
-					learningrate * err * grid[i][j].act * grid[nI][nJ].past_act
+					learningrate * err * grid[i][j].act * grid[nI][nJ].past_act * grid[i][j].memory * (1 - grid[nI][nJ].cooldown)
 				grid[i][j].weights[w] = grid[i][j].weights[w] +
-					learningrate * err * grid[i][j].act * grid[nI][nJ].act
+					learningrate * err * grid[i][j].act * grid[nI][nJ].act * (1 - grid[nI][nJ].cooldown)
 			end
 		end
 	end
@@ -99,6 +99,7 @@ function updateReproduction()
 	average_fitness = 0
 	for i = 1, GRID_WIDTH do
 		for j = 1, GRID_HEIGHT do
+			grid[i][j].act = grid[i][j].new_act
 			local neighbors = {
 				{x = i + 1, y = j, p = 1},
 				{x = i - 1, y = j, p = 1},
@@ -116,8 +117,7 @@ function updateReproduction()
 			local c = grid[i][j]
 			local selfFitness = 1
 				- math.tanh(math.min(c.act, c.past_act, c.past2_act))
---				- math.tanh(c.act * c.past_act * c.past2_act * 10)
---				- math.tanh(math.abs(c.err))^4
+				- math.tanh(math.abs(c.err))^4*2
 			local bestFitness = selfFitness
 
 			-- Iterate through neighbors to find the best fit
@@ -125,18 +125,15 @@ function updateReproduction()
 				local nI, nJ = wrapAroundGrid(neighbor.x, neighbor.y)
 				local n = grid[nI][nJ]
 				local fitness = 1
---					+ math.tanh(n.act)/2
 					-- Penalize static patterns and high activation
 					- math.tanh(math.min(n.act, n.past_act, n.past2_act))
 					-- Prevent flickering
-					- math.tanh(math.abs(n.err))^4
+					- math.tanh(math.abs(n.err))^4*2
 					-- Penalize genes
-					- (n.memory + 1)/(n.learning + 1)/32
 				fitness = fitness * neighbor.p
 					-- compare how well the neighbor predicts the center cell
-					* (	math.tanh( math.abs(c.act - c.past_act + 1) / (c.act + c.past_act + 1) )/
-						math.tanh( math.abs(c.act - n.past_act + 1) / (c.act + n.past_act + 1) ) )
---				if n.act == 0 then fitness = 0 end
+					* (	math.tanh( math.abs(c.act - c.past_act + 0.5) * (c.act + n.past_act + 0.5) )/
+						math.tanh( math.abs(c.act - n.past_act + 0.5) * (c.act + c.past_act + 0.5) ) )
 				if fitness > bestFitness then
 					bestFitness = fitness
 					bestNeighbors = {{x = nI, y = nJ}}
@@ -146,7 +143,7 @@ function updateReproduction()
 			end
 
 			-- Randomly select one of the best-fit neighbors
-			if bestFitness > math.max(0, selfFitness + c.cooldown) then
+			if bestFitness > math.max(0, selfFitness + c.cooldown + 0.2) then
 				local selectedNeighbor = bestNeighbors[math.random(1, #bestNeighbors)]
 				copyGenes(i, j, selectedNeighbor.x, selectedNeighbor.y)
 				c.cooldown = 1
@@ -383,20 +380,20 @@ function love.draw()
 			elseif view_mode == 3 then -- focus learning
 				red = math.tanh(grid[i][j].past_act)/16
 				green = math.tanh(grid[i][j].act)/16
-				blue = math.tanh(grid[i][j].learning*2)
+				blue = math.tanh(grid[i][j].learning)
 			elseif view_mode == 4 then -- focus color1
-				red = math.tanh(grid[i][j].color1*2)
+				red = math.tanh(grid[i][j].color1)
 				green = math.tanh(grid[i][j].past_act)/16
 				blue = math.tanh(grid[i][j].act)/16
 			elseif view_mode == 5 then -- focus memory
 				red = math.tanh(grid[i][j].act)/16
-				green = math.tanh(grid[i][j].memory*2)
+				green = math.tanh(grid[i][j].memory)
 				blue = math.tanh(grid[i][j].past_act)/16
-			elseif view_mode == 6 then -- focus memory
-				red = math.tanh(grid[i][j].color1*2)
-				green = math.tanh(grid[i][j].memory*2)
-				blue = math.tanh(grid[i][j].learning*2)
-			elseif view_mode == 7 then -- focus memory
+			elseif view_mode == 6 then -- genes
+				red = math.tanh(grid[i][j].color1)
+				green = math.tanh(grid[i][j].memory)
+				blue = math.tanh(grid[i][j].learning)
+			elseif view_mode == 7 then -- genes + activation
 				red = math.tanh(grid[i][j].color1/3 + grid[i][j].act/2 - grid[i][j].err/16)
 				green = math.tanh(grid[i][j].memory/3 + grid[i][j].past_act/2 - grid[i][j].err/16)
 				blue = math.tanh(grid[i][j].learning/3 + grid[i][j].act/2 + grid[i][j].past_act/4 + grid[i][j].past2_act/8)
